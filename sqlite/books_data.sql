0fd974447fb604b917f2e0d14381676ae124b346
anish-narain <69715492+anish-narain@users.noreply.github.com>
2022-11-17 09:55:27 +0000

    changes

---
+++ sqlite/books_data.sql
@@ -0,0 +1,46 @@
+INSERT INTO Book VALUES
+('978-2-16-148410-0', 'Crime and Punishment', 
+'Fyodor Dostoevsky');
+INSERT INTO Book VALUES
+('955-3-54-338410-0', 'Superintelligence', 
+'Nick Bostrom');
+INSERT INTO Book VALUES
+('953-5-23-378610-0', 'The Journal of Funny Science', 
+'Robert Spade editor');
+INSERT INTO Book VALUES
+('902-1-98-334410-0', 'The Periodical of Precautions', 
+'Peter Cautious editor');
+INSERT INTO Book VALUES
+('978-0-43-893210-0', 'Norwegian Wood', 'Haruki Murakami');
+INSERT INTO Book VALUES('978-9-22-148410-0', 'The Garlic Ballads', 'Mo Yan');
+INSERT INTO Book VALUES('953-7-21-334410-0', 'Alchemy', 'Marco Polo editor');
+INSERT INTO Book VALUES
+('902-4-44-365310-0', 'A Short History of Europe', 'Simon Jenkins');
+INSERT INTO Book VALUES
+('978-8-61-892810-0', 'Slaughterhouse-Five', 
+'Kurt Vonnegut');
+INSERT INTO Book VALUES
+('978-6-09-177710-0',
+'Sapiens: A Brief History of Humankind', 
+'Yuval Noah Harari');
+
+INSERT INTO Fiction VALUES
+('978-2-16-148410-0', 610, 'Drama', 1, 12.20);
+INSERT INTO Fiction VALUES('978-0-43-893210-0', 302, 'Drama', 1, 20.56);
+INSERT INTO Fiction VALUES('978-9-22-148410-0', 502, 'Historical', 0, 30.10); 
+INSERT INTO Fiction VALUES('978-8-61-892810-0', 170, 'War', 1, 7.5);
+
+INSERT INTO NonFiction VALUES
+('955-3-54-338410-0', 410, 'Science', 1, 0, 27.20);
+INSERT INTO NonFiction VALUES
+('902-4-44-365310-0', 350, 'History', 0, 1, 65.80);
+INSERT INTO NonFiction VALUES
+('978-6-09-177710-0', 450, 'Anthropology', 1, 0, 18.10); 
+
+INSERT INTO Periodical VALUES
+('953-5-23-378610-0', 420, 7, 21, 5.5);
+INSERT INTO Periodical VALUES
+('902-1-98-334410-0', 101, 5, 12, 2.5);
+INSERT INTO Periodical VALUES
+('953-7-21-334410-0', 190, 12, 13, 6.8);
+
